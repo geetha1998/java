64c36ec518afcf14fdba5d4762d6033507018687
geetha1998 <43902304+geetha1998@users.noreply.github.com>
2018-10-06 14:52:05 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
         if(num < 0)
             System.out.println(num + " is invalid");
-        else if(num % == 0)
+        else if(num % 2 == 0)
             System.out.println(num + " is even");
      else
       System.out.println(num + " is odd");
